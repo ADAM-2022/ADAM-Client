0d93c558af64b0e2bb2cb044a21a59104c80bcfd
feldblume5263 <feldblume5263@gmail.com>
2023-01-25 20:44:36 +0900

    feat: #2 project init

---
+++ ADAM/ADAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ADAM
+//
+//  Created by Noah Park on 2023/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
