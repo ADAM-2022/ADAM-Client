06b44015205aff6598d53a699d2db25e1dad841a
Taekhwan Han <hth3396@gmail.com>
2023-02-04 17:57:53 +0900

    feat: #9 BaseViewController 기본 요소 구현 navigationBar 설정 라이브러리 import 공통 메소드 선언

--- ADAM/ADAM/PresenterLayer/Base/BaseViewController.swift
+++ ADAM/ADAM/PresenterLayer/Base/BaseViewController.swift
@@ -6,11 +6,55 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
 
 class BaseViewController: UIViewController {
-
+    
+    //MARK: init
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setUpLayout()
+        configure()
         // Do any additional setup after loading the view.
+    }
+    
+    func setUpLayout() {
+        
+    }
+    
+    func configure() {
+        
+    }
+    
+    func setNavigationBar() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        let navigationAppearance = UINavigationBarAppearance()
+        navigationAppearance.configureWithDefaultBackground()
+        navigationAppearance.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 20, weight: .medium)]
+        navigationAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        navigationBar.compactAppearance = navigationAppearance
+        navigationBar.standardAppearance = navigationAppearance
+        navigationBar.scrollEdgeAppearance = navigationAppearance
+    }
+}
+
+
+extension BaseViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let viewControllersCount = navigationController?.viewControllers.count else { return false }
+        return viewControllersCount > 1
     }
 }
